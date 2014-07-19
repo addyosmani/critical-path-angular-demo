49d838dca5586ea526fa07e8fb028f133cd0434e
Addy Osmani <addyosmani@gmail.com>
2014-07-19 21:34:14 +0100

    Adding initial output

---
+++ app/scripts/main/main-ctrl.js
@@ -0,0 +1,59 @@
+'use strict';
+
+angular.module('criticalPathAngularDemo')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      {
+        'key': 'angular',
+        'title': 'AngularJS',
+        'url': 'https://angularjs.org/',
+        'description': 'HTML enhanced for web apps!',
+        'logo': 'angular.png'
+      },
+      {
+        'key': 'browsersync',
+        'title': 'BrowserSync',
+        'url': 'http://browsersync.io/',
+        'description': 'Time-saving synchronised browser testing.',
+        'logo': 'browsersync.png'
+      },
+      {
+        'key': 'gulp',
+        'title': 'GulpJS',
+        'url': 'http://gulpjs.com/',
+        'description': 'The streaming build system.',
+        'logo': 'gulp.png'
+      },
+      {
+        'key': 'jasmine',
+        'title': 'Jasmine',
+        'url': 'http://jasmine.github.io/',
+        'description': 'Behavior-Driven JavaScript.',
+        'logo': 'jasmine.png'
+      },
+      {
+        'key': 'karma',
+        'title': 'Karma',
+        'url': 'http://karma-runner.github.io/',
+        'description': 'Spectacular Test Runner for JavaScript.',
+        'logo': 'karma.png'
+      },
+      {
+        'key': 'protractor',
+        'title': 'Protractor',
+        'url': 'https://github.com/angular/protractor',
+        'description': 'End to end test framework for AngularJS applications built on top of WebDriverJS.',
+        'logo': 'protractor.png'
+      },
+      {
+        'key': 'bootstrap',
+        'title': 'Bootstrap',
+        'url': 'http://getbootstrap.com/',
+        'description': 'Bootstrap is the most popular HTML, CSS, and JS framework for developing responsive, mobile first projects on the web.',
+        'logo': 'bootstrap.png'
+      }
+    ];
+    angular.forEach($scope.awesomeThings, function(awesomeThing) {
+      awesomeThing.rank = Math.random();
+    });
+  });
---
+++ gulp/e2e-tests.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var gulp = require('gulp');
+
+var $ = require('gulp-load-plugins')();
+
+var browserSync = require('browser-sync');
+
+// Downloads the selenium webdriver
+gulp.task('webdriver-update', $.protractor.webdriver_update);
+
+gulp.task('webdriver-standalone', $.protractor.webdriver_standalone);
+
+gulp.task('protractor-only', ['webdriver-update', 'wiredep'], function (done) {
+  var testFiles = [
+    'test/e2e/**/*.js'
+  ];
+
+  gulp.src(testFiles)
+    .pipe($.protractor.protractor({
+      configFile: 'test/protractor.conf.js',
+    }))
+    .on('error', function (err) {
+      // Make sure failed tests cause gulp to exit non-zero
+      throw err;
+    })
+    .on('end', function () {
+      done();
+
+      // Close browser sync server
+      browserSync.instance.cleanup();
+      process.exit();
+    });
+});
+
+gulp.task('protractor', ['serve:e2e', 'protractor-only']);
+gulp.task('protractor:src', ['serve:e2e', 'protractor-only']);
+gulp.task('protractor:dist', ['serve:e2e-dist', 'protractor-only']);
